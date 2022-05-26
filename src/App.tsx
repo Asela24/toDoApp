3c23f82f7182b470961874c7da66480219e29197
selivanovskayaaa <selivanovskayaaa@akbarsdigital.ru>
2022-05-26 14:17:53 +0300

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+import './App.css';
+import { Button } from '@material-ui/core';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit and save to reload.
+        </p>
+      </header>
+      <Button>My button</Button>
+    </div>
+  );
+}
+
+export default App;
