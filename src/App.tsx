017eaf91afab8a623ee80797ce650b73f7b43c7b
selivanovskayaaa <selivanovskayaaa@akbarsdigital.ru>
2022-05-29 21:39:41 +0300

    create Task module

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 // eslint-disable-next-line no-use-before-define
+// @ts-ignore
 import React from 'react';
 import './App.css';
 import { Button } from '@material-ui/core';
+import { Task } from './shared/ui/task'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit and save to reload.
-        </p>
-      </header>
-      <Button>My button</Button>
+        <Task
+        timer={100}
+        title={'mytitle'}
+        isActive
+        isFinished
+        />
     </div>
   );
 }
--- src/shared/ui/task/Task.tsx
+++ src/shared/ui/task/Task.tsx
@@ -1,17 +1,25 @@
 import { FC } from 'react';
 import useStyle from './Task.styles';
+import PlayArrowOutlinedIcon from '@material-ui/icons/PlayArrowOutlined';
+import PauseOutlinedIcon from '@material-ui/icons/PauseOutlined';
+import DoneOutlinedIcon from '@material-ui/icons/DoneOutlined';
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined';
 
 // eslint-disable-next-line no-use-before-define,react/function-component-definition
 export const Task: FC<TaskProps> = ({
-  title, timer, isFinished, isActive,
+  title, timer, isFinished = false, isActive = false,
 }) => {
   const classes = useStyle();
-  return (
+    return (
     <div className={classes.taskContainer}>
       <div className={classes.informContainer}>
-        <div className={classes.taskTitle}>{title}</div>
-          <div className={classes.taskTitle}>{timer}</div>
+          <div className={classes.taskTitle}>{title}</div>
+          <div className={classes.taskTimer}>{timer}</div>
       </div>
+        <div className={classes.actionsContainer}>
+            {isActive ? <PlayArrowOutlinedIcon fontSize='inherit' color='action'/> : <PauseOutlinedIcon fontSize='inherit'/>}
+            {isFinished ? <DoneOutlinedIcon fontSize='inherit'/> : <CloseOutlinedIcon fontSize='inherit' /> }
+        </div>
     </div>
   );
 };
